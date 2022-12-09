ca174b5bc9ee601107b6a12b7435347bb3685f22
Mixail_Sorokin <ip.sorokin1738@mail.ru>
2022-12-09 23:53:21 +0300

    correct

--- main.py
+++ main.py
@@ -1,55 +1,56 @@
-from sympy import *
-import matplotlib.pyplot as plt
-import math
-# a=float(input('X1: '))
-# b=float(input('X2: '))
-# y1 = -12*(a**4)*math.sin(math.cos(a))-18*(a**3)+5*(a**2)+10*a-30
-# y2 = -12*(b**4)*math.sin(math.cos(b))-18*(b**3)+5*(b**2)+10*b-30
+import scipy
+import numpy as np
+from  graph import Graph
+Graph()
+list_roots=[]
 
-# if a>b and y1>y2:
-#     print(f'Функция в промежутке от {b} до {a} возростает')
-# elif a<b and y1>y2:
-#     print(f'Функция в промежутке от {a} до {b} убывает')
-# else:
-#     print('Введите промежуток поменьше')
-# print(y1,y2)
+def func1(x,x1):
+    return (-12*(x**4)*np.sin(np.cos(x))-18*(x**3)+5*(x**2)+10*x-30)
+t0=1
+min=-10
+max=0
+x1=scipy.optimize.brentq(func1, min,max,args=(t0))
 
 
-# def roots(a, b, c):
-#     D = b ** 2 - 4 * a * c
-#     d = D ** 0.5
-#     x1 = (-b + d) / (2 * a)
-#     x2 = (-b - d) / (2 * a)
-#     print(d)
-#     if D > 0:
-#         return x1, x2
-#     elif x1 == x2:
-#         return x1
-#     else:
-#         exit('Complex roots')
+def func2(x,x2,):
+    return (-12*(x**4)*np.sin(np.cos(x))-18*(x**3)+5*(x**2)+10*x-30)
+t0=1
+min=0
+max=10
+x2=scipy.optimize.brentq(func2, min,max,args=(t0))
 
-# x=Symbol('x')
 
-# k1, k2, k3 = 12*(x*x)*sin(cos(x))-18*x+5, 10, -30
-
-# roots = roots(k1, k2, k3)
-# print(roots)
+list_roots.append(x1)
+list_roots.append(x2)
+f = lambda x: round(x,2)
+list_roots = list(map(f,list_roots))
+print(f'Корни функции [x1,x2] = {list_roots}')
 
 list_x = list(range(-200, 0))
 f = lambda x: x/100
 list_x = list(map(f, list_x))
-print(f'(x)= {list_x} \n')
 
-f = lambda x: -12*(x**4)*math.sin(math.cos(x))-18*(x**3)+5*(x**2)+10*x-30
+
+f = lambda x: -12*(x**4)*np.sin(np.cos(x))-18*(x**3)+5*(x**2)+10*x-30
 list_y = list(map(f, list_x))
-# list_y=list(list_y)
-# list_y.sort()
-print(f'f(x)= ',list_y)
+
+roots_new=[]
+roots_new.append(list_roots[0]-1)
+roots_new.append(round((list_roots[0]+list_roots[1])/2,2))
+roots_new.append(list_roots[1]+1)
+
+list_y_new=list(map(f,roots_new))
+
+sign=lambda x: ('f(x)>=0 ') if x>0 else 'f(x)<0 '
+sign=list(map(sign,list_y_new))
 
 
-
-
-
-
-
-
+sign_new=[]
+sign_new.append(sign[0])
+sign_new.append(list_roots[0])
+sign_new.append(sign[1])
+sign_new.append(list_roots[1])
+sign_new.append(sign[2])
+print(sign_new)
+print(f'На промежутке (-4 , {list_roots[0]}] и [{list_roots[1]}, +4) f(x)>=0 \n\
+На промежутке [{list_roots[0]}, {list_roots[1]}] f(x)<0')
